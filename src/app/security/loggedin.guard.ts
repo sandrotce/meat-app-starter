1d506197e4cb4435f3ae3d3487a99a3e36010ff1
Alessandro Afonso <alessandro.afonso@totvs.com.br>
2021-07-05 14:35:08 -0300

    Alteração para quando o usuario não estiver logado, chama a login() 1) alterado loggedin.guard 2) login.service

--- src/app/security/loggedin.guard.ts
+++ src/app/security/loggedin.guard.ts
@@ -1,14 +1,26 @@
 import {CanLoad, Route} from '@angular/router'
 import {Injectable} from '@angular/core'
 
+import {LoginService} from './login/login.service'
+
 @Injectable()
 
 export class LoggedInGuard implements CanLoad {
 
+  constructor(private loginService : LoginService){}
 
   canLoad(route : Route) : boolean {
-    console.log(route)
-    return false
+
+    let loggedIn = this.loginService.isLoggedIn()
+
+    if (!loggedIn)
+       this.loginService.handleLogin()
+    else{
+      return loggedIn
+    }
+
+    //console.log(route)
+
   }
 
 }
--- src/app/security/login/login.service.ts
+++ src/app/security/login/login.service.ts
@@ -1,6 +1,8 @@
 import {Injectable} from '@angular/core'
 import {HttpClient} from "@angular/common/http"
 import {Observable} from 'rxjs/Observable'
+import {Router} from '@angular/router'
+
 import 'rxjs/add/operator/map'
 import 'rxjs/add/operator/do'
 
@@ -13,7 +15,7 @@ export class LoginService{
 
   user : User
 
-  constructor(private http: HttpClient){}
+  constructor(private http: HttpClient, private router : Router){}
 
   isLoggedIn() : boolean {
     return this.user !== undefined
@@ -25,4 +27,11 @@ export class LoginService{
                        {email : email, password : password} )
                       .do(user => this.user = user) // desta forma com o do() conseguimos pegar o retorno do metodo post e gravar no this.user.
   }
+
+  handleLogin(){
+
+    this.router.navigate(['/login'])
+
+  }
+
 }
